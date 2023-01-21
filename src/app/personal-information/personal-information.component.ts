93d4d102f48b67661efd1abf8d5a8fb1e9d5b916
VashisthDev <107829512+VashisthDev@users.noreply.github.com>
2023-01-21 15:53:12 +0530

    **

--- src/app/personal-information/personal-information.component.ts
+++ src/app/personal-information/personal-information.component.ts
@@ -9,16 +9,15 @@ export class PersonalInformationComponent implements OnInit {
   myData: string[][] = [
     ['Name', 'Dev Vashisth'],
     ['DOB', '22/06/2003'],
-    ['Work Exp', '0 Years'],
+    
     ['Education', 'B.Tech(2025)'],
-    ['Interests', 'We go Jim 🔱'],
+    ['Interests', 'Football'],
   ];
 
   aboutMe: string[] = [
-    'Hi, I am a freelance web developer and student with a strong background in creating high-quality websites.',
-    'I have experience working with a variety of programming languages such as HTML, CSS, Typescript and frameworks like Angular .',
-    'I am a quick learner and can adapt to new technologies easily.',
-    'As a student, I am constantly expanding my knowledge and experience in web development, which allows me to bring fresh ideas and innovative solutions to my projects.',
+    'Hey, my name is Dev Vashisth. I’m a graphic designer, UX/UI designer & front-end web developer from India.',
+     'I’m also passionate about pop music and football and I’m always curious to learn more when it comes to new technologies and creative coding.',
+    
   ];
 
   constructor() {}
